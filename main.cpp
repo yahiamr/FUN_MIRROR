5487a3f911baa50670e4024ae0ef448664194480
yahiamr <146677961+yahiamr@users.noreply.github.com>
2023-11-18 23:11:52 +0200

    Update file path

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <opencv2/opencv.hpp>
 
 int main() {
-    cv::Mat image = cv::imread("./image.jpg");
+    cv::Mat image = cv::imread("../image.jpg");
     if (image.empty()) {
         std::cout << "Could not read the image" << std::endl;
         return 1;
